0dc16618aab6de05545e04b73bfab3e215604787
Priya Mohan <priyavajram2303@gmail.com>
2025-08-29 12:17:41 +0530

    Fix import: added/renamed search.jsx file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export {Movielist} from "./Movielist";
 export {Moviedetails} from "./Moviedetails";
-export {Search} from "./search";
+export {Search} from "./Search";
 export {RateMovie} from "./RateMovie"
 export {Pagenotfound} from "./Pagenotfound";
